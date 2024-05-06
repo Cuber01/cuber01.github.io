089e312957c25c0178455eb8d5ec571ea69473b2
Cuber01 <johnmallard631@gmail.com>
2024-05-06 12:41:31 +0200

    Fonts preload

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,6 +46,17 @@ export default function RootLayout({
         'text-black bg-white dark:text-white dark:bg-black',
       )}
     >
+      <link rel="preload" href="/fonts/oxygen-v15-latin-300.woff2" as="font" type="font/woff2"/>
+      <link rel="preload" href="/fonts/oxygen-v15-latin-300.ttf" as="font" type="font/ttf"/>
+      <link rel="preload" href="/fonts/oxygen-v15-latin-700.woff2" as="font" type="font/woff2"/>
+      <link rel="preload" href="/fonts/oxygen-v15-latin-700.ttf" as="font" type="font/ttf"/>
+      <link rel="preload" href="/fonts/oxygen-v15-latin-regular.woff2" as="font" type="font/woff2"/>
+      <link rel="preload" href="/fonts/oxygen-v15-latin-regular.ttf" as="font" type="font/ttf"/>
+      <link rel="preload" href="/fonts/poppins-v21-latin-500.woff2" as="font" type="font/woff2"/>
+      <link rel="preload" href="/fonts/poppins-v21-latin-500.tff" as="font" type="font/tff"/>
+      <link rel="preload" href="/fonts/poppins-v21-latin-600.woff2" as="font" type="font/woff2"/>
+      <link rel="preload" href="/fonts/poppins-v21-latin-600.ttf" as="font" type="font/tff"/>
+
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
