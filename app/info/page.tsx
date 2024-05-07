297ee8bd0b4f5e787d1efa6bd4f13037577f0c89
Cuber01 <johnmallard631@gmail.com>
2024-05-07 11:04:52 +0200

    Added info page

---
+++ app/info/page.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link"
+
+
+export const metadata = {
+  title: 'Info'
+}
+
+export default function Page() {
+    return (
+      <section>
+        <h1 className="poppins text-2xl font-semibold tracking-tighter">
+         Jan Englert 
+       </h1>                
+       <h1 className="poppins mb-8 text-xl font-semibold tracking-tighter">
+         <Link href="https://github.com/Cuber01">Github</Link>
+         {` — `}
+         <Link href="https://cuber1.itch.io/">Itch.io</Link>
+         {` — `}
+         <Link href="mailto:janenglert098@icloud.com">Email</Link>
+       </h1>
+
+       <p className="mb-4">
+         {`I'm a self-taught programmer with a knack for learning new things. 
+         I have many interests including game and language design, writing, tabletop RPGs,
+          history and everything tech. My blog will likely focus on all these things, but currently I have written mostly things about sci-fi.`}
+       </p>
+
+      </section>
+     
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -11,19 +11,5 @@ export default function Page() {
         <div id="welcome"  >Jan Englert</div>
         </div>
       </div>
-    // <section>
-    //   <h1 className="text-2xl font-semibold tracking-tighter">
-    //     Jan Englert 
-    //   </h1>                
-    //   <h2 className="mb-8 text-base font-semibold tracking-tighter">
-    //     @Cuber01
-    //   </h2>
-
-    //   <p className="mb-4">
-    //     {`I'm a self-taught programmer with a knack for learning new things. 
-    //     I have many interests including game and language design, writing, tabletop RPGs,
-    //      history and everything tech. `}
-    //   </p>
-    // </section>
   )
 }
