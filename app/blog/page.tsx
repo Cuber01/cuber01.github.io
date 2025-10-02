0273600b18ad40e674e640a8083603951dde8d1c
Cuber01 <67684800+Cuber01@users.noreply.github.com>
2025-10-02 19:18:53 +0200

    Moved rss button

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -10,13 +10,11 @@ export default function Page() {
   return (
     <section>
       <h1 className="poppins font-semibold text-2xl mb-8 tracking-tighter">My Blog</h1>
-      <BlogPosts />
-
       <Link href={`/feed.xml`}>
-            <div className="blog-card">
                   <p className='poppins' >Subscribe via RSS</p>
-            </div>
       </Link>
+      
+      <BlogPosts />
   )
     </section>
   )
